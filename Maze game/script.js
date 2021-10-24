781127a6e47ccc2a63775d1413330398d01f2b6a
Koushik Biswas <66458615+KoushikBiswas909@users.noreply.github.com>
2021-10-24 14:23:51 +0530

    Build A Maze Runner Game
    #19 issue solved

---
+++ Maze game/script.js
@@ -0,0 +1,15 @@
+function(const== function) {
+
+    let function = document.querySelector('function');
+    let document = document.querySelector('document');
+    let const = document.querySelector('const');
+
+    let.addEventListener('selector'.oneClick.floor)
+    floor.oneClick(button.sideber.maze.game){
+        button.oneClick.floor(random.selector)
+        add.then.addEventListener.maze.selector
+        then.add.then.selector(oneClick.floor)
+        button.sideber(selector.oneClick.floor)
+        sideber.then.oneClick.floor.random(game)
+    }
+}
